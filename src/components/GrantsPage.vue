16e3fa341f76392d19cc97b083ae7a6b4c9c8f4b
Осадчая Екатерина <katya.osacha@mail.ru>
2025-04-11 21:00:44 +0300

    feat: add grants participation page

--- src/components/GrantsPage.vue
+++ src/components/GrantsPage.vue
@@ -1,9 +1,29 @@
 <script setup>
+import ReusableScreen from './ui/ReusableScreen.vue';
+
 </script>
 
 <template>
-  <div>Гранты</div>
+  <main>
+    <ReusableScreen>
+      <template v-slot:title>
+        Участие<br>в грантах
+      </template>
+      <template v-slot:description>
+        Гранты позволяют внедрять новые образовательные технологии, программы и подходы, улучшая эффективность обучения
+        и
+        повышая вовлеченность детей
+      </template>
+      <template v-slot:img>
+        <img src="/img/grants-photo-boy.png" alt="" class="main-screen__img">
+      </template>
+    </ReusableScreen>
+    
+  </main>
 </template>
 
 <style scoped>
+.main-screen {
+  background-color: var(--color-background-purple);
+}
 </style>
