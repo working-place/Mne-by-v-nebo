7c04dbdbd12ce0c417a9b74a2a7b3dd95e38c98c
ViktoriaElm <v.gorkina.job@gmail.com>
2025-04-28 10:33:35 +0300

    fix: scrollBehavior top to landing page after click link

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
