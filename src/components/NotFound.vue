be480ff3dbf9b6a7b14eeac591f037dcaa4cb738
Осадчая Екатерина <katya.osacha@mail.ru>
2025-05-21 19:47:42 +0300

    fix: fixed the image path on page 404

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -8,7 +8,7 @@ import RouterLinkButton from './ui/RouterLinkButton.vue';
     <main>
         <div class="not-found">
             <h1>Ошибка 404</h1>
-            <InfoCard imageUrl="public\img\404-photo-boy.png" title="ОЙ!  Кажется, этой страницы тут нет"
+            <InfoCard imageUrl="/img/404-photo-boy.png" title="ОЙ!  Кажется, этой страницы тут нет"
                 description="Страница не найдена.  Мы тоже расстроены и делаем все возможное, чтобы исправить проблему. А пока попробуйте поискать что-нибудь другое или вернитесь на главную." />
 
                 <RouterLinkButton :to="{ name: 'home' }" :disabled="false" max-height-btn="63px" min-height-btn="63px"
